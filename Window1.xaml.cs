7d401c444a0a3553b424dd35a13ddfbf782697f6
Nikita <Nikita@DNS>
2020-06-25 11:54:48 +0500

    Добавьте файлы проекта.

---
+++ Window1.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Data;
+
+namespace Ekz
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOKDate_Click(object sender, RoutedEventArgs e)
+        {
+            //using (DataContext db = new DataContext(Properties.Settings.Default.srConnectionString))
+            //{
+            //    btnKl
+            //    string date = dgklient.Text;
+            //    Table<klients> uslugis = db.GetTable<klients>();
+            //    Table<klients> klients = db.GetTable<klients>();
+            //    var query = from a in zakazs
+            //                join b in klients on a.id_klzak equals b.id_kl
+            //                join c in uslugis on a.id_uslzak equals c.id_uslugi
+            //                where a.date_zak == Convert.ToDateTime(date)
+            //                select new { a.id_zakaz, a.date_zak, a.status_zak, b.id_kl, c.name_uslugi, c.cena_uslugi };
+            //    dgklient.ItemsSource = query;
+            //}
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+        public void Update()
+        {
+            using (DataContext db = new DataContext(Properties.Settings.Default.srConnectionString))
+            {
+
+                Table<klients> klients = db.GetTable<klients>();
+                dgklient.ItemsSource = klients;
+
+            }
+        }
+
+        private void btnNewKl_Click(object sender, RoutedEventArgs e)
+        {
+            tbFIONew.IsEnabled = true;
+            btnCanKl.IsEnabled = true;
+            btnSaveNewKl.Visibility = Visibility.Visible;
+            
+        }
+
+        private void btnSaveNewKl_Click(object sender, RoutedEventArgs e)
+        {
+            DataClasses2DataContext db = new DataClasses2DataContext();
+            klients klients = new klients();
+            klients.name_kl = tbFIONew.Text;
+            db.GetTable<klients>().InsertOnSubmit(klients);
+            db.SubmitChanges();
+            Update();
+            tbFIONew.Text = "";
+            tbFIONew.IsEnabled = false;
+            btnCanKl.IsEnabled = false;
+        }
+
+        private void btnRedKl_Click(object sender, RoutedEventArgs e)
+        {
+            tbFIONew.IsEnabled = true;
+            btnCanKl.IsEnabled = true;
+            btnSaveNewKl.Visibility = Visibility.Hidden;
+            btnSaveRedKl.Visibility = Visibility.Visible;
+            DataClasses2DataContext db = new DataClasses2DataContext();
+            object item = dgKlient.SelectedItem;
+            long vb = Convert.ToInt64((dgKlient.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text);
+            tbFIONew.Text = (from u in db.klients
+                             where u.id_kl == vb
+                             select u.name_kl).FirstOrDefault();
+        }
+
+        private void btnCanKl_Click(object sender, RoutedEventArgs e)
+        {
+            tbFIONew.Text = "";
+        }
+
+        private void btnKl_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                using (DataContext db = new DataContext(Properties.Settings.Default.srConnectionString))
+                {
+                    Table<klients> q = db.GetTable<klients>();
+                    var srcTable = q.Where(p => p.name_kl.Contains(btnKl.Text));
+                    dgklient.ItemsSource = srcTable;
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void btnSaveRedKl_Click(object sender, RoutedEventArgs e)
+        {
+            DataClasses2DataContext db = new DataClasses2DataContext();
+            klients klients = new klients();
+            klients.name_kl = tbFIONew.Text;
+            db.GetTable<klients>().InsertOnSubmit(klients);
+            db.SubmitChanges();
+            Update();
+
+            tbFIONew.Text = "";
+            tbFIONew.IsEnabled = false;
+            btnCanKl.IsEnabled = false;
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Data;
+
+namespace Ekz
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void btZareg_Click(object sender, RoutedEventArgs e)
+        {
+            string s = tbPassReg.Text;
+            char[] array = s.ToCharArray();
+            int d = s.Length;
+            int k = 0;
+            int u = 0;
+            int b = 0;
+            char p = '$';
+            char j = '!';
+            char f = '@';
+            char h = '%';
+            char z = '^';
+            char x = '#';
+            for (int i = 0; i < d; i++)
+            {
+                if (char.IsUpper(array[i]))
+                    k++;
+
+            }
+            for (int i = 0; i < d; i++)
+            {
+                if (char.IsNumber(array[i]))
+                    u++;
+
+            }
+            for (int i = 0; i < d; i++)
+            {
+                if (Convert.ToChar(p) == (array[i]) || Convert.ToChar(j) ==
+                (array[i]) || Convert.ToChar(f) == (array[i]) || Convert.ToChar(h) == (array[i]) ||
+                Convert.ToChar(z) == (array[i]) || Convert.ToChar(x) == (array[i]))
+                    b++;
+            }
+            if ((k >= 1) && (tbPassReg.Text.Length >= 6) && (u >= 1) && (b >= 1) && tbFIOReg.Text.Length != 0)
+            {
+                using (DataContext db = new DataContext(Properties.Settings.Default.srConnectionString))
+                {
+                    string fio = tbFIOReg.Text;
+                    string log = tbLogReg.Text;
+                    string pas = tbPassReg.Text;
+                    string rol = tbRolReg.Text;
+                    users user = new users();
+                    user.fio_user = fio;
+                    user.lod_user = log;
+                    user.pas_user = pas;
+                    user.rol_user = rol;
+                    db.GetTable<users>().InsertOnSubmit(user);
+                    db.SubmitChanges();
+                    MessageBox.Show("Пользователь добавлен");
+                    btnBack.Visibility = Visibility;
+                    btZareg.Visibility = Visibility.Hidden;
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Пароль должен содержать специальные символы ($ ! @ # ^ %), одну цифру и одну заглавную букву, а так же должен состоять минимум из 6 символов");
+            }
+        }
+    
+            private void btnBack_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                this.Hide();
+            }
+
+
+        
+}   }
